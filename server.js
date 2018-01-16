dae48cbbcdf9e2c19a0f82716069fb5cdea878a4
Audrey Wong <audreydotwong@gmail.com>
2018-01-15 21:47:37 -0500

    more changes to test.js

--- server.js
+++ server.js
@@ -27,15 +27,15 @@ app.use(morgan('common')); //use morgan to log
 app.use(bodyParser.json()); //Parse everything as json format
 
 //Where to serve static content
-app.use( express.static('public') );
+app.use(express.static('public'));
 
 app.get("/", (request, response) => {
-  response.sendFile(__dirname + '/public/index.html');
+    response.sendFile(__dirname + '/public/index.html');
 });
 
 
 //Database config
-mongoose.connect(config.databaseUrl, {useMongoClient: true});
+mongoose.connect(config.databaseUrl, { useMongoClient: true });
 db.on('error', console.error.bind(console, 'Connection error:'));
 db.once('open', () => {
     console.log('Connected to a database')
@@ -57,48 +57,31 @@ app.listen(port, () => {
 let server;
 
 // this function connects to our database, then starts the server
-function runServer(databaseUrl = config.databaseUrl, port=config.localPort) {
+function runServer() {
 
-  return new Promise((resolve, reject) => {
-    mongoose.createConnection(databaseUrl, { useMongoClient: true }, err => {
-      if (err) {
-        return reject(err);
-      }
-      server = app.listen(port, () => {
-        console.log(`Your app is listening on port ${port}`);
-        resolve();
-      })
-      .on('error', err => {
-        mongoose.disconnect();
-        reject(err);
-      });
+    mongoose.connect(config.databaseUrl, { useMongoClient: true });
+    db.on('error', console.error.bind(console, 'Connection error:'));
+    db.once('open', () => {
+        console.log('Connected to a database')
     });
-  });
 }
 
 // this function closes the server, and returns a promise. we'll
 // use it in our integration tests later.
 function closeServer() {
-  return mongoose.disconnect().then(() => {
-     return new Promise((resolve, reject) => {
-       console.log('Closing server');
-       server.close(err => {
-           if (err) {
-               return reject(err);
-           }
-           resolve();
-       });
-     });
-  });
+    mongoose.disconnect();
+    db.once('close', () => {
+    	console.log('Closed database');
+    })
 }
 
 // if server.js is called directly (aka, with `node server.js`), this block
 // runs. but we also export the runServer command so other code (for instance, test code) can start the server as needed.
 if (require.main === module) {
-  runServer().catch(err => console.error(err));
+    runServer().catch(err => console.error(err));
 };
 
-module.exports = {app, runServer, closeServer};
+module.exports = { app, runServer, closeServer };
 
 //4.- to run the server -> node server.js
 //5.- With nodemon now its nodemon server.js and will listen to any change
--- test/test.js
+++ test/test.js
@@ -15,7 +15,13 @@ const { databaseUrl } = require('../config');
 chai.use(chaiHttp);
 
 describe('Get landing page', function() {
+  before(function() {
+    return runServer();
+  });
   
+  after(function() {
+    return closeServer();
+  });
 
   it('should return 200 status code with html', function() {
     return chai.request(app)
