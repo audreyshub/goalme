48d8a4f1f544533a7b35f9252c4e25b74811628c
Audrey Wong <audreydotwong@gmail.com>
2018-01-15 20:38:26 -0500

    changes to test.js

--- test/test.js
+++ test/test.js
@@ -15,6 +15,13 @@ const { TEST_DATABASE_URL } = require('../config');
 chai.use(chaiHttp);
 
 describe('Get landing page', function() {
+  before(function() {
+    return runServer();
+  });
+
+  after(function() {
+    return closeServer();
+  });
   
   it('should return 200 status code with html', function() {
     return chai.request(app)
